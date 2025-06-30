c337532bd3ed3392e12fae5d580d4f6974e366ca
AbuTaher003 <136100360+AbuTaher003@users.noreply.github.com>
2025-06-30 23:32:32 +0600

    Add files via upload

--- Codes/Bisection.py
+++ Codes/Bisection.py
@@ -1,30 +1,28 @@
 import math
+
 def f(x):
-    return 3*x - math.cos(x) - 1   # এখানে নিজের function বসাও
+    return 3*x - math.cos(x) - 1
 
-def bisection(a, b, iterations):
+def Bisection(a,b,iteration):
     if f(a) * f(b) >= 0:
-        print("Invalid interval: f(a) and f(b) must have opposite signs.")
+        print("Invalid a and b")
         return None
-
+        
     print("initial interval : a =",a , "and b =",b)
 
-    for step in range(1, iterations + 1):
-        c = (a + b) / 2
-        print(f"Step {step}: c = {c:.6f}, f(c) = {f(c):.6f}")
+    for step in range (1, iteration+1):
+        c = (a+b)/2
+        print(f"step-{step:.6f} c={c:.6f} f(c)={f(c):.6f}")
 
         if f(c) == 0:
-            # exact root found
             break
-        elif f(a) * f(c) < 0:
-            b = c
-        else:
+        elif f(a) * f(c) > 0:
             a = c
+        else:
+            b = c
 
     return c
 
-
-# Example usage
-iterations = 8
-root = bisection(a=0, b=1, iterations=iterations)
-print(f"\nEstimated root after {iterations} iterations: {root}")
+iteration = 8
+root = Bisection(a = 0, b = 1, iteration = iteration)
+print(f"After {iteration} iterations the root is : {root}")
