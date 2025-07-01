1bb631eaab3383a6487ee124bcac3da1c174e471
AbuTaher003 <136100360+AbuTaher003@users.noreply.github.com>
2025-07-02 01:25:08 +0600

    Add files via upload

--- Codes/Newton_Raphson.py
+++ Codes/Newton_Raphson.py
@@ -1,30 +1,29 @@
 import math
 
 def f(x):
-    return 3*x - math.cos(x) - 1 # give your function here.
+    return 3*x - math.cos(x) -1
 
 def df(x):
-    return 3 + math.sin(x) # derivative of f(x).
+    return 3 + math.sin(x)
 
 def Newton_Raphson(x0, iteration):
+    print("Initial Guess x0 = ",x0,"\n")
 
-    print("initial guess, x0: ",x0)
-    
-    for i in range(1, iteration+1):
+    for i in range(1,iteration+1):
         fx = f(x0)
         dfx = df(x0)
 
         if dfx == 0:
-            print("Zero derivative, Method fails.")
+            print("Zero derivation, method fails.")
+            return None
 
         x1 = x0 - fx/dfx
 
-        print(f"Step-{i}: x = {x1:.6f}, f(x) = {f(x1):.6f}")
+        print(f"step-{i} : x = {x1:.6f}, f(x) = {f(x1):.6f}")
 
         x0 = x1
 
     return x1
 
-
-root = Newton_Raphson(x0=0,iteration=10)
-print("the root is: ",root)
+root = Newton_Raphson(x0=0, iteration=5)
+print(f"\nAfter 5 iterations the root is :",root)
